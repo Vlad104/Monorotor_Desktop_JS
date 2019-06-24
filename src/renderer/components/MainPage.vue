21602105d66e23041fa93ac6b3ece955c9c08cfc
Vlad104 <vlad104khk@gmail.com>
2019-06-24 03:11:17 +0300

    Start app development

---
+++ src/renderer/components/MainPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="main">
+    <input type="text" value="Объём">
+    <input type="text" value="Подача">
+    <input type="text" value="Ускорение">
+    <input type="text" value="Отношение А">
+    <input type="text" value="Отношение Б">
+    <input type="text" value="Передаточное А">
+    <input type="text" value="Передаточное Б">
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'main-page',
+    methods: {
+    }
+  }
+</script>
+
+<style>
+
+#main {
+    display: flex;
+    flex-direction: column;
+    width: 200px;
+}
+
+#main > * {margin: 10px 0 10px 0;}
+
+</style>
